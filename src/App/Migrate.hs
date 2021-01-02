9c6541b1e831329e0a9a70904d274bab5fac6255
Rune K. Svendsen <runesvend@gmail.com>
2021-01-02 17:16:25 +0100

    print current schema before migrating

--- src/App/Migrate.hs
+++ src/App/Migrate.hs
@@ -17,11 +17,13 @@ import Data.Proxy (Proxy(..))
 import Database.Beam.Postgres
 import Database.Beam.Schema
 import qualified Database.Beam.AutoMigrate as BA
+import qualified Database.Beam.AutoMigrate.Postgres as PgBA
 import qualified Control.Monad.Reader as R
 import qualified Control.Monad.State.Strict as S
 import Control.Monad.Except (unless, runExceptT)
 import Control.Monad.Trans.Except (ExceptT)
 import Control.Exception (throwIO)
+import qualified Text.Show.Pretty as PP
 
 
 autoMigrateIO :: Config -> IO ()
@@ -34,7 +36,8 @@ autoMigrate = do
     withDbConn $ \conn -> lift $ runBeamPostgresDebug (runAppM cfg . logDebug "MIGRATE") conn $ do
         let migration = BA.migrate conn hsSchema
         isMigrated <- isMigratedM migration
-        unless isMigrated $
+        unless isMigrated $ do
+            S.liftIO (printCurrentSchema conn)
             S.liftIO (runMigration conn migration)
   where
     annotatedDb :: BA.AnnotatedDatabaseSettings Postgres LiquidityDb
@@ -48,6 +51,13 @@ runMigration conn migration = do
     Log.logInfo "MIGRATE" "----------------------------------------------------"
     S.liftIO $ BA.printMigrationIO migration
     S.liftIO $ BA.runMigrationUnsafe conn migration
+
+printCurrentSchema :: Connection -> IO ()
+printCurrentSchema conn = do
+    Log.logInfo "MIGRATE" "----------------------------------------------------"
+    Log.logInfo "MIGRATE" "CURRENT SCHEMA:"
+    PgBA.getSchema conn >>= PP.pPrint
+    Log.logInfo "MIGRATE" "----------------------------------------------------"
 
 isMigratedM :: BA.Migration Pg -> Pg Bool
 isMigratedM m = do
