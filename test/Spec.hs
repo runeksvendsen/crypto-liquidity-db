62f092b84c45df6609d30edff83128d60086ec12
Rune K. Svendsen <runesvend@gmail.com>
2021-02-24 15:00:03 +0100

    test: fix test exit code + unit test case

--- test/Spec.hs
+++ test/Spec.hs
@@ -7,6 +7,8 @@ import           OrderBook.Graph.Internal.Prelude
 import           Test.HUnit
 import qualified Test.Hspec.Runner                  as Run
 import           Test.Hspec                         (parallel)
+import qualified Test.Hspec as Hspec
+import Test.Hspec.Contrib.HUnit (fromHUnitTest)
 
 import System.Environment (lookupEnv)
 import Data.Maybe (fromMaybe)
@@ -16,7 +18,9 @@ main :: IO ()
 main = App.Main.Util.withDbPool App.Main.Util.LevelDebug $ \pool -> do
     baseUrl <- readBaseUrl >>= Process.Spec.mkClientEnv
     done <- Process.Spec.setup pool
-    void $ runTestTT $ Process.Spec.tests baseUrl done
+    Hspec.hspec $
+        Hspec.describe "Unit tests" $
+            fromHUnitTest $ Process.Spec.tests baseUrl done
 
 readBaseUrl :: IO String
 readBaseUrl = do
